6b4ca04a4be555f49d8a1122243338b219f6af5c
July <myiszhb@gmail.com>
2021-05-08 15:24:26 +0800

    1

--- springcloud-service-portal-01/src/main/java/com/july/springcloud/PortalApplicatin.java
+++ springcloud-service-portal-01/src/main/java/com/july/springcloud/PortalApplicatin.java
@@ -21,6 +21,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class PortalApplicatin {
     public static void main(String[] args) {
         SpringApplication.run(PortalApplicatin.class,args);
-        log.info("portal服务（消费者）已启动，端口：8080");
+        log.info("portal服务（消费者）已启动，端口：8000");
     }
 }
